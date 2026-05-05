59e8f7cc2bc4437500c0a4299feadb1d602a9852
SULTANUM MOBIN <sultanum.mobin@gmail.com>
2026-05-05 23:39:44 +0600

    code lab 1

--- 01 Convert RGB to Grayscale image/code.py
+++ 01 Convert RGB to Grayscale image/code.py
@@ -9,7 +9,7 @@ r_const, g_const, b_const = 0.2126, 0.7152, 0.0722
 
 grayscale_image = r_const * r ** gamma + g_const * g ** gamma + b_const * b ** gamma
 
-fig = plt.figure()
+fig = plt.figure(figsize=(15, 10))
 img1, img2 = fig.add_subplot(121), fig.add_subplot(122)
 img1.imshow(input_image)
 img2.imshow(grayscale_image, cmap=plt.cm.get_cmap('gray'))
--- 01 Convert RGB to Grayscale image/code_2.py
+++ 01 Convert RGB to Grayscale image/code_2.py
@@ -6,20 +6,19 @@ import matplotlib.pyplot as plt
 image = Image.open('sm_pic.jpg').convert('RGB')
 # Convert the RGB image to Grayscale
 gray_image = image.convert('L')
-# Convert grayscale image to numpy array
+
 gray_matrix = np.array(gray_image)
-# Display the original and grayscale images
+
 plt.figure(figsize=(10, 5))
 # Original image
 plt.subplot(1, 2, 1)
 plt.title('Original RGB Image')
 plt.imshow(image)
-# plt.axis('off')
+
 # Grayscale image
 plt.subplot(1, 2, 2)
 plt.title('Grayscale Image')
 plt.imshow(gray_image, cmap='gray')
-# plt.axis('off')
 plt.show()
 # Display the grayscale image matrix
 print("Grayscale Image Matrix:")
